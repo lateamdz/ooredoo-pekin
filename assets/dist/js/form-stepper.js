37422c9154b029ea259c5ffebf6e8004ff942fab
samikhammar <khammar73@gmail.com>
2023-10-14 13:16:16 +0100

    condidature-page

--- assets/dist/js/form-stepper.js
+++ assets/dist/js/form-stepper.js
@@ -26,6 +26,7 @@ class FormStepper{
         }
     };
     headerItems;
+    contentItems;
     prevBtn;
     nextBtn;
     stepCount;
@@ -35,6 +36,7 @@ class FormStepper{
         this.props=props;
 
         this.headerItems=document.getElementById(this.id).getElementsByClassName("form-stepper-count");
+        this.contentItems=document.getElementById(this.id).getElementsByClassName("form-stepper-content-step");
         this.prevBtn=document.getElementById(this.id).querySelector('[data-role="prev"]');
         this.nextBtn=document.getElementById(this.id).querySelector('[data-role="next"]');
         this.stepCount=this.headerItems.length;
@@ -42,7 +44,6 @@ class FormStepper{
         //     // Do something with 'element'
         // });
         this.useEffect();
-        console.log(this.nextBtn,this.prevBtn);
     }
 
     useEffect(){
@@ -52,10 +53,26 @@ class FormStepper{
         classToggle(this.prevBtn,{
             "d-none":this.currentStep===this.stepCount?this.props.global.hidePrevOnLastStep:this.currentStep<=1
         });
+
+
+        this.nextBtn.innerHTML=this.props[this.currentStep]?.nextText|| this.props.global.defaultNextText;
+        this.prevBtn.innerHTML=this.props[this.currentStep]?.prevText|| this.props.global.defaultPrevText;
+
+        for(let i=1;i<=this.stepCount;i++){
+            classToggle(this.headerItems[i-1],{
+                "passed": i<=this.currentStep
+            });
+        }
+
+        for(let i=1;i<=this.stepCount;i++){
+            classToggle(this.contentItems[i-1],{
+                "d-none": i!==this.currentStep
+            });
+        }
+
         this.nextBtn.onclick=()=>{
             if(this.currentStep<this.stepCount){
                 this.currentStep++;
-                console.log(this.currentStep);
                 this.useEffect();
             }
         };
@@ -63,19 +80,9 @@ class FormStepper{
         this.prevBtn.onclick=()=>{
             if(this.currentStep>1){
                 this.currentStep--;
-                console.log(this.currentStep);
                 this.useEffect();
             }
         };
-
-        this.nextBtn.innerHTML=this.props[this.currentStep]?.nextText|| this.props.global.defaultNextText;
-        this.prevBtn.innerHTML=this.props[this.currentStep]?.prevText|| this.props.global.defaultPrevText;
-        for(let i=1;i<this.stepCount;i++){
-            classToggle(this.headerItems[i],{
-                "passed": this.currentStep>i
-            });
-
-        }
     }
 
 }
